2b77e7b0d681c587522bd50a66aedbc391ef356f
VanditaPramod <vanditapramod07@gmail.com>
2024-11-29 00:34:34 +0530

    intitial commit

---
+++ src/main/java/com/qa/utils/TestUtil.java
@@ -0,0 +1,8 @@
+package com.qa.utils;
+
+public class TestUtil {
+
+	public void TestUtil() {
+		System.out.println("Test Util");
+	}
+}
---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,10 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	@Test
+	public void LoginTest() {
+		System.out.println("Login test");
+	}
+}
